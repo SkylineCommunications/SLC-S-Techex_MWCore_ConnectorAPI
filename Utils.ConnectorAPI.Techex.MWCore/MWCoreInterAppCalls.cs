67ccf754546f80945552bc3ceb5beccc539221e2
Samuel Duarte <samuel.duarte@skyline.be>
2024-06-26 12:02:25 +0100

    MWCoreInterAppCalls constructors

--- Utils.ConnectorAPI.Techex.MWCore/MWCoreInterAppCalls.cs
+++ Utils.ConnectorAPI.Techex.MWCore/MWCoreInterAppCalls.cs
@@ -8,6 +8,7 @@ namespace Skyline.DataMiner.Utils.ConnectorAPI.Techex.MWCore
     using Skyline.DataMiner.Core.InterAppCalls.Common.CallBulk;
     using Skyline.DataMiner.Core.InterAppCalls.Common.Shared;
     using Skyline.DataMiner.Net;
+    using Skyline.DataMiner.Net.Messages;
     using Utils.ConnectorAPI.Techex.MWCore.InterAppMessages;
 
     /// <summary>
@@ -25,6 +26,88 @@ namespace Skyline.DataMiner.Utils.ConnectorAPI.Techex.MWCore
 
         /// <inheritdoc/>
         public int ElementId { get; }
+
+        /// <summary>
+		/// Initialize a new instance of the <see cref="MWCoreInterAppCalls"/> class.
+		/// </summary>
+		/// <param name="connection">The connection interface.</param>
+		/// <param name="elementName">The name of the element in DataMiner.</param>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException"></exception>
+		public MWCoreInterAppCalls(IConnection connection, string elementName)
+        {
+            if (String.IsNullOrEmpty(elementName))
+            {
+                throw new ArgumentException("Please provide a valid Element name.", nameof(elementName));
+            }
+
+            SLNetConnection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            ElementInfoEventMessage elementInfo;
+            try
+            {
+                elementInfo = (ElementInfoEventMessage)SLNetConnection.HandleSingleResponseMessage(new GetElementByNameMessage
+                {
+                    ElementName = elementName,
+                });
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException($"The element does not exists with name '{elementName}'", nameof(elementName));
+            }
+
+            if (elementInfo.Protocol != Constants.ProtocolName)
+            {
+                throw new ArgumentException($"The element is not running protocol '{Constants.ProtocolName}'", nameof(elementName));
+            }
+
+            AgentId = elementInfo.DataMinerID;
+            ElementId = elementInfo.ElementID;
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="MWCoreInterAppCalls"/> class.
+        /// </summary>
+        /// <param name="connection">The connection interface.</param>
+        /// <param name="dmaId">The id of the DataMiner that is hosting the element.</param>
+        /// <param name="elementId">The id of the element in DataMiner.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public MWCoreInterAppCalls(IConnection connection, int dmaId, int elementId)
+        {
+            if (dmaId == default)
+            {
+                throw new ArgumentException("Please provide a valid DMA ID.", nameof(dmaId));
+            }
+
+            if (elementId == default)
+            {
+                throw new ArgumentException("Please provide a valid Element ID.", nameof(elementId));
+            }
+
+            SLNetConnection = connection ?? throw new ArgumentNullException(nameof(connection));
+            AgentId = dmaId;
+            ElementId = elementId;
+
+            ElementInfoEventMessage elementInfo;
+            try
+            {
+                elementInfo = (ElementInfoEventMessage)SLNetConnection.HandleSingleResponseMessage(new GetElementByIDMessage
+                {
+                    DataMinerID = dmaId,
+                    ElementID = elementId,
+                });
+            }
+            catch
+            {
+                throw new ArgumentException($"The element does not exists with id '{dmaId}/{elementId}'", nameof(elementId));
+            }
+
+            if (elementInfo.Protocol != Constants.ProtocolName)
+            {
+                throw new ArgumentException($"The element is not running protocol '{Constants.ProtocolName}'", nameof(elementId));
+            }
+        }
 
         /// <inheritdoc/>
         public void SendMessageNoResponse(params IMWCoreRequest[] messages)
